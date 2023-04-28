b2f5e1a8dd5024e38cc099e95cbcaf6fc201235a
Guilherme Oliveira Casagrande <guigacasa@protonmail.ch>
2023-04-28 11:16:31 -0300

    first commit

---
+++ src/test/java/br/espm/guilherme/cotacao/CotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.espm.guilherme.cotacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CotacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
